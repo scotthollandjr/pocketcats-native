c13e52d0c46107b21b7bd6ae667b6ca7ed0bab42
Scott Holland <scott.holland@eyecuelab.com>
2017-06-06 23:08:22 -0700

    Update map to display realm cat list, remove hard coded cats

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -31,169 +31,6 @@ let popupHidden = true;
 let cats = realm.objects('Cat');
 let catsArray = cats.map(x => Object.assign({}, x));
 
-let fakeCats = [
-  {
-    id: 0,
-    name: "Appa",
-    type: "Siamese",
-    tagged: false,
-    gender: "Male",
-    notes: "Very aggressive, blue eyes",
-    age: 4,
-    verified: false,
-    owner: null,
-    image: "https://s-media-cache-ak0.pinimg.com/736x/58/2b/fa/582bfac8dab69c53984c2fa8642db942.jpg",
-    coordinate: {
-      longitude: -122.6804352795281,
-      latitude: 45.49031028995796,
-    }
-  },
-  {
-    id: 1,
-    name: "Eli",
-    type: "Black",
-    tagged: false,
-    gender: "Male",
-    notes: "Crooked ear and cuts on back",
-    age: 7,
-    verified: false,
-    owner: null,
-    image: "https://s-media-cache-ak0.pinimg.com/736x/85/a7/39/85a739f5737ceced2aedbb8f31178d74.jpg",
-    coordinate: {
-      longitude: -122.6927766289744,
-      latitude: 45.52556019729369,
-    }
-  },
-  {
-    id: 2,
-    name: "Breakfast",
-    type: "Maine Coon",
-    tagged: true,
-    gender: "Female",
-    notes: "Loud meowing",
-    age: 3,
-    verified: false,
-    owner: null,
-    image: "https://pbs.twimg.com/profile_images/506522217052528640/wBUjUJSK_400x400.jpeg",
-    coordinate: {
-      longitude: -122.6799167354337,
-      latitude: 45.52846642077944,
-    }
-  },
-  {
-    id: 3,
-    name: "Quee",
-    type: "Mix",
-    tagged: true,
-    gender: "Unknown",
-    notes: "Fighting other cats in neighborhood",
-    age: null,
-    verified: false,
-    owner: null,
-    image: "https://pbs.twimg.com/profile_images/708067740888801280/7rTsSuBP.jpg",
-    coordinate: {
-      longitude: -122.6789833560638,
-      latitude: 45.52120058053362,
-    }
-  },
-  {
-    id: 4,
-    name: "Chester",
-    type: "Orange Tabby",
-    tagged: true,
-    gender: "Male",
-    notes: "Seems to be indoor cat",
-    age: null,
-    verified: false,
-    owner: null,
-    image: "https://pbs.twimg.com/profile_images/668439459986350081/gfVktJWg.jpg",
-    coordinate: {
-      longitude: -122.6861392645663,
-      latitude: 45.51553257367652,
-    }
-  },
-  {
-    id: 5,
-    name: "Riff-Raff",
-    type: "Orange Tabby",
-    tagged: false,
-    gender: "Male",
-    notes: "Fat and chunky. Mrow",
-    age: 13,
-    verified: false,
-    owner: null,
-    image: "https://ih1.redbubble.net/image.73670090.0092/flat,1000x1000,075,f.u2.jpg",
-    coordinate: {
-      longitude: -122.6968212729106,
-      latitude: 45.51989262962709,
-    }
-  },
-  {
-    id: 6,
-    name: "Lil' Douggie",
-    type: "Tabby",
-    tagged: false,
-    gender: "Male",
-    notes: "Seems to be an indoor cat",
-    age: 1,
-    verified: false,
-    owner: null,
-    image: "https://s-media-cache-ak0.pinimg.com/236x/1b/92/e2/1b92e2949becda1f180d46fc4b546690.jpg",
-    coordinate: {
-      longitude: -122.6551303277222,
-      latitude: 45.52011062355635,
-    }
-  },
-  {
-    id: 7,
-    name: "Feli",
-    type: "Black",
-    tagged: false,
-    gender: "Unknown",
-    notes: "None",
-    age: null,
-    verified: false,
-    owner: null,
-    image: "https://s-media-cache-ak0.pinimg.com/originals/95/ed/a7/95eda7740e1745ccd6ac85250e7b42f6.jpg",
-    coordinate: {
-      longitude: -122.6635307420512,
-      latitude: 45.53260752981592,
-    },
-  },
-  {
-    id: 8,
-    name: "Zoe",
-    type: "Tortoise Shell",
-    tagged: true,
-    gender: "Female",
-    notes: "Lorem ipsum dolar",
-    age: null,
-    verified: false,
-    owner: null,
-    image: "https://s-media-cache-ak0.pinimg.com/736x/34/ba/db/34badbcd09831ed25743cb2850177737.jpg",
-    coordinate: {
-      longitude: -122.6758720914976,
-      latitude: 45.53979925769482,
-    }
-  },
-  {
-    id: 9,
-    name: "Bohdi",
-    type: "Tabby",
-    tagged: false,
-    gender: "Male",
-    notes: "Caught eating dim sum",
-    age: 13,
-    verified: false,
-    owner: null,
-    image: "https://www.sturbridgeyankee.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/r/brown_tabby_cat_1.jpg",
-    coordinate: {
-      longitude: -122.6698569800027,
-      latitude: 45.50717485546119,
-    }
-  }
-]
-
 class MainMap extends Component {
   componentWillMount() {
     navigator.geolocation.getCurrentPosition(
@@ -254,10 +91,8 @@ class MainMap extends Component {
   }
 
   onMarkerClick(cat) {
-    let lat = cat.coordinate.latitude - .01;
-    let long = cat.coordinate.longitude + .01;
-
-
+    let lat = cat.location.latitude - .01;
+    let long = cat.location.longitude + .01;
 
     this.setState({ region: {
       latitude: lat,
@@ -290,7 +125,7 @@ class MainMap extends Component {
               Type: {cat.type}
             </Text>
             <Text style={styles.popupText2}>
-              Notes: {cat.notes}
+              {cat.description}
             </Text>
           </View>
         </TouchableWithoutFeedback>
@@ -310,7 +145,7 @@ class MainMap extends Component {
             {cats.map(cat => (
               <MapView.Marker
                 key={cat.id}
-                coordinate={cat.coordinate}
+                coordinate={cat.location}
                 title={cat.first_name}
                 onPress={() => this.onMarkerClick(cat)}
                 >
