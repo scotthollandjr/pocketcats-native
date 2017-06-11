233ea7c4cb5b8a48f36a68f1ab561b9c6a22a96f
Scott Holland <scott.holland@eyecuelab.com>
2017-06-10 22:05:35 -0700

    Add nav buttons to landing screen

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Actions, ActionConst } from 'react-native-router-flux';
 import { Button } from './common';
 
 class LandingPage extends Component {
+  onMapButtonPress() {
+    Actions.main();
+  }
+  onLoginButtonPress() {
+    Actions.auth();
+  }
+  onSignupButtonPress() {
+    Actions.auth({type:ActionConst.RESET});
+    Actions.signup();
+  }
+
   render() {
     return (
-      <View>
-        <Text>LANDING</Text>
+      <View style={{flex:1}}>
+        <Button
+          text="MAP"
+          onPress={this.onMapButtonPress.bind(this)}
+        >
+        </Button>
+        <Button
+          text="LOGIN"
+          onPress={this.onLoginButtonPress.bind(this)}
+        >
+        </Button>
+        <Button
+          text="SIGNUP"
+          onPress={this.onSignupButtonPress.bind(this)}
+        >
+        </Button>
       </View>
     )
   }
